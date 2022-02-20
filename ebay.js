fa76c62e76b7b14f41f0ddc01c830366801f0d9f
EzraNagel <97614103+EzraNagel@users.noreply.github.com>
2022-02-19 21:41:18 -0500

    Update ebay.js

--- ebay.js
+++ ebay.js
@@ -5,28 +5,60 @@ function fillAddress(){
     address = address.replace(",","");
     var array = address.replace(/\r/g, "").split(/\n/);
 
-
-    if(lineNum == 3){
-        array1 = array[0].split(" ");
-        array2 = array[2].split(" ");
+    document.querySelector('[data-test-id="EXPAND_SHIPPING_ADDRESSES"]').click();
+    setTimeout(function(){
+        document.querySelector('[data-test-id="GET_ADDRESS_FORM"]').removeAttribute("data-test-id");
+        document.querySelector('[data-test-id="GET_ADDRESS_FORM"]').click();
+    setTimeout(function(){  
         
+        if(lineNum == 3){
+            array1 = array[0].split(" ");
+            array2 = array[2].split(" ");
+        
+            name(array1);
+            document.getElementById('addressLine1').value = array[1];
+            document.getElementById('addressLine2').value = "";
+            city(array2);
+    
+
+        } else{
+            array1 = array[0].split(" ");
+            array2 = array[3].split(" ");
+        
+            name(array1);
+            document.getElementById('addressLine1').value = array[1];
+            document.getElementById('addressLine2').value = array[2];
+            city(array2);
+        }
+        
+        setTimeout(function(){
+            document.querySelector('[data-test-id="EDIT_ADDRESS_SUBMIT"]').click();
+            setTimeout(function(){
+                alert(address);
+                setMessage();
+            }, 2000);
+        }, 2000);
+        
+    }, 2000);
+        
+    }, 2000);
+}
+
+function name(nameLine){
+    console.log(nameLine);
+    if (nameLine.length == 1){
+        document.getElementById('firstName').value = array1[0];
+        document.getElementById('lastName').value = ".";
+    }
+    else if (nameLine.length == 2){
         document.getElementById('firstName').value = array1[0];
         document.getElementById('lastName').value = array1[1];
-        document.getElementById('addressLine1').value = array[1];
-        document.getElementById('addressLine2').value = "";
-        city(array2);
-        document.getElementByDataTestId('')
-
-    } else{
-        array2 = array[3].split(" ");
-    
-        document.getElementById('firstName').value = array1[0];
-        document.getElementById('firstName').value = array1[1];
-        document.getElementById('addressLine1').value = array[1];
-        document.getElementById('addressLine2').value = array[2];
-        city(array2);
     }
-    message();
+    else if (nameLine.length == 3){
+        document.getElementById('firstName').value = array1[0];
+        document.getElementById('lastName').value = array1[1] + " " + array2[2];
+    }
+
 }
 
 function city(addressLine){
@@ -43,3 +75,14 @@ function city(addressLine){
 }
 
 fillAddress();
+
+function setMessage(){
+    setTimeout(function(){
+        document.querySelector('[data-test-id="MESSAGE_TO_SELLER_OPEN"]').click();
+    }, 2000);
+
+    alert("Thank you for this item! This is being sent to someone else, so please do not include any invoices or ebay materials. Thanks!");
+   
+}
+
+
